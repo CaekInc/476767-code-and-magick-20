0422ccd4bf71c80c14d32d06d769f1b44f9cda14
Caek <caekinc@gmail.com>
2020-06-17 13:53:40 +0300

    refresh

--- js/setup.js
+++ js/setup.js
@@ -6,14 +6,9 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['де Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_NUMBER = 4;
 var similarListElement = setupBlock.querySelector('.setup-similar-list');
 var similatWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-
-// var randomInteger = function (min, max) {
-//   var rand = min + Math.random() * (max - min + 1);
-//   return Math.round(rand);
-// };
 
 var randomInteger = function (min, max) {
   min = Math.ceil(min);
@@ -21,66 +16,48 @@ var randomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getWizardName = function () {
-  var name = WIZARD_NAMES[randomInteger(0, WIZARD_NAMES.length)];
-  var surname = WIZARD_SURNAMES[randomInteger(0, WIZARD_SURNAMES.length)];
-  return name + ' ' + surname;
+var getRandomElementFrom = function (array) {
+  return array[randomInteger(0, array.length)];
 };
 
-var getWizardCoat = function () {
-  return WIZARD_COATS[randomInteger(0, WIZARD_COATS.length)];
+
+var getRandomWizard = function () {
+  var wizard = {
+    name: getRandomElementFrom(WIZARD_NAMES) + ' ' + getRandomElementFrom(WIZARD_SURNAMES),
+    coatColor: getRandomElementFrom(WIZARD_EYES),
+    eyesColor: getRandomElementFrom(WIZARD_COATS)
+  };
+  return wizard;
 };
 
-var getWizardEyeColor = function () {
-  return WIZARD_EYES[randomInteger(0, WIZARD_EYES.length)];
-};
-
-var wizards = [
-  {
-    name: getWizardName(),
-    coatColor: getWizardCoat(),
-    eyesColor: getWizardEyeColor()
-  },
-  {
-    name: getWizardName(),
-    coatColor: getWizardCoat(),
-    eyesColor: getWizardEyeColor()
-  },
-  {
-    name: getWizardName(),
-    coatColor: getWizardCoat(),
-    eyesColor: getWizardEyeColor()
-  },
-  {
-    name: getWizardName(),
-    coatColor: getWizardCoat(),
-    eyesColor: getWizardEyeColor()
+var getWizards = function (numbers) {
+  var wizards = [];
+  for (var i = 0; i < numbers; i++) {
+    wizards.push(getRandomWizard());
   }
-];
+  return wizards;
+};
 
-// var renderWizard = function (wizard) {
-//   var wizardElement = similatWizardTemplate.cloneNode(true);
-//   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-//   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-//   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-// };
 
-// var fragment = document.createDocumentFragment();
-// for (var i = 0; i < wizards.length; i++) {
-//   fragment.appendChild(renderWizard(wizards[i]));
-// }
-
-// similarListElement.appendChild(fragment);
-
-//  Это способ почему то не работает. Оставил посмотреть в чем проблема.
-
-for (var i = 0; i < wizards.length; i++) {
+var renderWizard = function (wizard) {
   var wizardElement = similatWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
 
-  similarListElement.appendChild(wizardElement);
-}
+
+var renderFragment = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  return fragment;
+};
 
 setupBlock.querySelector('.setup-similar').classList.remove('hidden');
+var wizards = getWizards(WIZARDS_NUMBER);
+var wizardFragment = renderFragment(wizards);
+similarListElement.appendChild(wizardFragment);
